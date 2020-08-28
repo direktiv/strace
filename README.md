213f6cd743bc17c8efd992f8bd49d56bfce61891
Jens Gerke <jens.gerke@vorteil.io>
2020-08-28 15:40:46 +1000

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # strace
+
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/ef616f8f26d4458dbb2addae9cfe4e09)](https://www.codacy.com/gh/vorteil/strace?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=vorteil/strace&amp;utm_campaign=Badge_Grade)
--- pkg/strace/tracker_test.go
+++ pkg/strace/tracker_test.go
@@ -13,13 +13,6 @@ func TestTracker(t *testing.T) {
 	newTracker, err := NewTracker(testCommand)
 	assert.NoError(t, err, fmt.Sprintf("error creating new tracker"))
 
-	lgAssert := func(format string, values ...interface{}) {
-		assert.NotEmpty(t, fmt.Sprintf(format, values...), "captured Sys call was empty")
-		t.Logf(format, values...) // In case user wants to see strace output
-	}
-
-	TraceLogInfo = lgAssert
-
 	fin, err := newTracker.Start()
 	assert.Error(t, err, fmt.Sprintf("error starting tracker"))
 	assert.True(t, fin, "tracker did not finish gracefully")
